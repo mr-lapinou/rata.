04b9a35e88bac775a9739b6dd2198352f3705852
mrlapinou <thibaultperson@gmail.com>
2014-02-09 21:58:17 +0100

    better ui + lucky wheel

--- website/public/js/slot.js
+++ website/public/js/slot.js
@@ -2,44 +2,79 @@ var rolling =false;
 var numberitems = 0;
 var doneitems=0;
 var rolls=0;
-var maxroll;
+var maxroll=1;
 var animation_time=2;
-var colorscheme= ["#6e4c5a","#eb5490","#82b941","#e34a2e","#3a817d"];
-var colorschemeband= ["#4F3742","#eb5490","#82b941","#e34a2e","#3a817d"];
+var colorscheme= ["#6e4c5a","#d15490","#a8ba42","#fc872e","#3a817d"];
+var colorschemeband= ["#4F3742","#9b4070","#8a9335","#c96928","#2e635f"];
+var timerPanelID;
+
+var spinvalue;
 
 $(document).ready(function(){
 	$(document).keydown(function(e){
 		console.log(e.which);
-		if(e.which==38) spinUp();
-		else if (e.which==40) spinDown();
+		if(e.which==38) spin(-1);
+		else if (e.which==40) spin(1);
 	});
-
-
 });
 
+
+function luckywheel(){
+	if (rolling) return;
+	rolling =true;
+	cancelPanelOpening();
+	numberitems = $(".recipelist div").length+1;
+	maxroll=numberitems*2+Math.floor(Math.random()*numberitems+1);
+	closePanel(spinDown);
+}
+
 function changeContent (type){
-	
+	closePanel(animationLoad(type));
+}
+function animationLoad(type){
 	$("#centercontainer").hide( 'slide', { direction : 'right'  }, 300, function(){
 		$("#menu > ul > .selected").removeClass("selected");
 		$('body' ).animate({ backgroundColor: colorscheme[type-1]}, 500,function(){
-			$(".hasback").css({ "background-color": colorschemeband[type-1]});
+			$(".secondcolor").css({ "background-color": colorschemeband[type-1]});
+			$("#rightpanel").css('background-image', 'url(img/filigrane-0' + type + '.png)');
 			$("#menu > ul >#menu"+type).addClass("selected");
 			$("#centercontainer").show( 'slide', { direction : 'right'  }, 300);
 		});
 		
 	});
-	
+	requestPanelOpening();	
+}
+
+function closePanel(callback){
+	$("#rightpanel").hide( 'slide', { direction : 'right'  }, 300,callback);
+}
+
+function openPanel(){
+	$("#rightpanel").show( 'slide', { direction : 'right'  }, 300);
+}
+
+function requestPanelOpening(){
+	timerPanelID = window.setTimeout(openPanel, 700);
+}
+
+function cancelPanelOpening(){
+	window.clearTimeout(timerPanelID);
+}
+
+
+function spin(delta){
+	if (rolling) return;
+	rolling =true;
+	cancelPanelOpening();
+	if(delta<0)
+		closePanel(spinUp);
+	else
+		closePanel(spinDown);
 }
 
 
 
-
-
-
-
-function spinDown(){
-	if (rolling) return;
-	rolling =true;
+function spinDown(){	
 	numberitems = $(".recipelist div").length+4;
 	doneitems =0;
 	var l = $("#upperlist > .recipe").last().clone();
@@ -51,9 +86,10 @@ function spinDown(){
 	l.removeClass("highlight");
 	$("#lowerlist").prepend(l);
 	$("#lowerlist > .recipe").css({"top": "-1.2em"});
+var time  =  (maxroll>1)?animation_time:200;
 	$("#highlightcontainer > .recipe").css({"top": "-1.2em"});
 	$(".recipe").each(function(){
-		$(this).animate({ "top": "+=1.2em" },200,function(){
+		$(this).animate({ "top": "+=1.2em" },time,function(){
 			doneitems++;
 			if(doneitems == numberitems){
 				$("#lowerlist > .recipe").last().remove();				
@@ -70,8 +106,6 @@ function spinDown(){
 
 
 function spinUp(){
-	if (rolling) return;
-	rolling =true;
 	numberitems = $(".recipelist div").length+4;
 	doneitems =0;	
 	var l = $("#lowerlist > .recipe").first().clone();
@@ -84,8 +118,9 @@ function spinUp(){
 	$("#upperlist").append(l);
 	$("#highlightcontainer > .recipe").css({"top": "0em"});
 	$("#upperlist > .recipe").css({"top": "1.2em"});
+	var time  =  (maxroll>1)?animation_time:200;
 	$(".recipe").each(function(){
-		$(this).animate({ "top": "-=1.2em" },200,function(){
+		$(this).animate({ "top": "-=1.2em" },time,function(){
 			doneitems++;
 			if(doneitems == numberitems){
 				$("#lowerlist > .recipe").first().remove();				
@@ -99,30 +134,19 @@ function spinUp(){
 		});
 	});
 }
-
-function finalizeSpin(){
-	doneitems++;
-	if(doneitems == numberitems){
-		$("#lowerlist > .recipe").last().remove();				
-		$("#highlightcontainer > .recipe").last().remove();
-		$("#upperlist > .recipe").last().remove();
-		
-		$(".recipe").each(function(){
-			$(this).css({ "top": "0em" });
-		});
-		speedDown();
-	}
-
-}
 	
 
 function speedDown(){
 	rolls++;
-	animation_time = easeInQuint(rolls, 2000, 3000,maxroll);
-	if(rolls<1){
-		spin();
+	animation_time = easeInQuint(rolls, 20, 200,maxroll);
+	if(rolls<maxroll){
+		cancelPanelOpening();
+		spinDown();
 	}else{
 		rolling = false;
+		requestPanelOpening();
+		rolls=0;
+		maxroll=1;
 	}
 		
 
